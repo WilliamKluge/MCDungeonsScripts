a34e7702877e9ddf92affa3d961c98da4abc07aa
WilliamKluge <klugewilliam@gmail.com>
2020-06-03 16:05:29 -0500

    added some configuration options

--- control/constant_ghost_form.py
+++ control/constant_ghost_form.py
@@ -4,26 +4,28 @@ import pyautogui
 import keyboard
 
 
-def __ghost_form_thread_function():
-    while not keyboard.is_pressed('f2'):
+def __ghost_form_thread_function(player_cooldown):
+    ghost_cloak_cooldown = 6
+    while not keyboard.is_pressed('f7'):
         pyautogui.keyDown('1')
         pyautogui.keyUp('1')
-        time.sleep(1.3)
+        time.sleep((ghost_cloak_cooldown*player_cooldown)/2)
         pyautogui.keyDown('2')
         pyautogui.keyUp('2')
-        time.sleep(1.3)
+        time.sleep((ghost_cloak_cooldown*player_cooldown)/2)
 
 
-def __ghost_form_speed_thread_function():
-    while not keyboard.is_pressed('f2'):
+def __ghost_form_speed_thread_function(player_cooldown):
+    boots_cooldown = 5
+    while not keyboard.is_pressed('f7'):
         pyautogui.keyDown('3')
         pyautogui.keyUp('3')
-        time.sleep(5*.48)
+        time.sleep((boots_cooldown*player_cooldown))
 
 
-def constant_ghost_form():
-    ghost_thread = threading.Thread(target=__ghost_form_thread_function)
-    speed_thread = threading.Thread(target=__ghost_form_speed_thread_function)
+def constant_ghost_form(player_cooldown):
+    ghost_thread = threading.Thread(target=__ghost_form_thread_function, args=[player_cooldown])
+    speed_thread = threading.Thread(target=__ghost_form_speed_thread_function, args=[player_cooldown])
     ghost_thread.start()
     speed_thread.start()
     ghost_thread.join()
@@ -31,6 +33,8 @@ def constant_ghost_form():
 
 
 if __name__ == '__main__':
-    print('Registering hotkey for ghost form on f1')
-    keyboard.add_hotkey('f1', constant_ghost_form)
+    player_cooldown_percent = 1 - (int(input('Enter your total cooldown percentage as a whole number (be nice or it '
+                                             'will crash, not validating this): ')) / 100)
+    print('Registering hotkey for ghost form on f6')
+    keyboard.add_hotkey('f6', constant_ghost_form, args=[player_cooldown_percent])
     keyboard.wait()
