540f1f0d7354bc3d2c2d589fc8a79168c9e9a31d
WilliamKluge <klugewilliam@gmail.com>
2020-06-03 17:53:42 -0500

    Duplicate item fix

--- control/constant_ghost_form.py
+++ control/constant_ghost_form.py
@@ -25,6 +25,19 @@ def slot_thread_func(player_cooldown, slot_cooldown, key):
         time.sleep(slot_cooldown * cooldown_adjustment)
 
 
+def duplicate_item_thread_func(player_cooldown, slot_cooldown, keys):
+    global stop_threads
+    cooldown_adjustment = 1 - player_cooldown
+    current_index = 0
+    while not stop_threads:
+        pyautogui.keyDown(keys[current_index])
+        pyautogui.keyUp(keys[current_index])
+        time.sleep((slot_cooldown * cooldown_adjustment) / len(keys))
+        current_index += 1
+        if current_index > len(keys):
+            current_index = 0
+
+
 def smart_cooldown_spammer():
     global stop_threads
     print('Reading values from PlayerConfig.ini')
@@ -36,17 +49,33 @@ def smart_cooldown_spammer():
         slot_one_cooldown = int(config['DEFAULT']['Slot1Cooldown'])
         slot_two_cooldown = int(config['DEFAULT']['Slot2Cooldown'])
         slot_three_cooldown = int(config['DEFAULT']['Slot3Cooldown'])
-        s1t = threading.Thread(target=slot_thread_func, args=[player_cooldown, slot_one_cooldown, '1'])
-        s2t = threading.Thread(target=slot_thread_func, args=[player_cooldown, slot_two_cooldown, '2'])
-        s3t = threading.Thread(target=slot_thread_func, args=[player_cooldown, slot_three_cooldown, '3'])
+        slot_one_item = config['DEFAULT']['Slot1Item']
+        slot_two_item = config['DEFAULT']['Slot2Item']
+        slot_three_item = config['DEFAULT']['Slot3Item']
+        threads = []
+        if slot_one_item != slot_two_item and slot_one_item != slot_three_item:
+            s1t = threading.Thread(target=slot_thread_func, args=[player_cooldown, slot_one_cooldown, '1'])
+            threads.append(s1t)
+        else:
+            keys = ['1']
+            if slot_one_item == slot_two_item:
+                keys.append('2')
+            if slot_one_item == slot_three_item:
+                keys.append('3')
+            dst = threading.Thread(target=duplicate_item_thread_func, args=[player_cooldown, slot_one_cooldown, '1'])
+            threads.append(dst)
+        if slot_two_item != slot_one_item:
+            s2t = threading.Thread(target=slot_thread_func, args=[player_cooldown, slot_two_cooldown, '2'])
+            threads.append(s2t)
+        if slot_three_item != slot_one_item:
+            s3t = threading.Thread(target=slot_thread_func, args=[player_cooldown, slot_three_cooldown, '3'])
+            threads.append(s3t)
         stop_threads_thread = threading.Thread(target=stop_threads_func)
-        s1t.start()
-        s2t.start()
-        s3t.start()
+        for thread in threads:
+            thread.start()
         stop_threads_thread.start()
-        s1t.join()
-        s2t.join()
-        s3t.join()
+        for thread in threads:
+            thread.join()
         stop_threads_thread.join()
         print('Threads joined - hotkey ready')
     except:
